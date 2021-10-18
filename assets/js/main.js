6bdf4c6e95720ead350cfda6ca9e79e08f0085a3
iamfrego <mate_freg@tiscali.it>
2021-10-18 14:40:47 +0200

    jsnack 3 completo

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+//jSnack1-----------------------------------------------------------------------
+
+// const jsnack1 = document.querySelector("div.jSnack1");
+
+// const firstNumber = parseInt(prompt("Inserisci un numero"));
+// const secondNumber = parseInt(prompt("Inserisci un numero"));
+
+// if (firstNumber > secondNumber) {
+//     const results = `<p> Il primo numero è il maggiore </p>`
+//     jsnack1.innerHTML = results;
+
+// } else if (secondNumber > firstNumber) {
+//     const results = `<p> Il secondo numero è il maggiore </p>`
+//     jsnack1.innerHTML = results;
+
+// } else {
+//     const results = `<p> I numeri sono uguali </p>`
+//     jsnack1.innerHTML = results;
+// }
+
+//jSnack2-----------------------------------------------------------------------
+
+//const jsnack2 = document.querySelector("div.jSnack2");
+//
+//const firstWord = prompt("Inserisci una parola");
+//const secondWord = prompt("Inserisci una parola");
+//
+//if (firstWord.length >= secondWord.length) {
+//    const results = `<p> Parola 1 ${firstWord}  Parola 2 ${secondWord} </p>`
+//    jsnack1.innerHTML = results;
+//
+//} else {
+//    const results = `<p> Parola 2 ${secondWord} Parola 1 ${firstWord} </p>`
+//    jsnack2.innerHTML = results;
+//}
+
+//jSnack3-----------------------------------------------------------------------
+
+// const jsnack3 = document.querySelector("div.jSnack3");
+// let counter = 0;
+
+// for (let i = 0; i < 10; i++) {
+//     const number = parseInt(prompt(`Inserisci un numero -- ${i}`));
+//     counter += number;
+// }
+
+// const results = `<p> ${counter} </p>`;
+// jsnack3.innerHTML = results;
+
+//jSnack4-----------------------------------------------------------------------
+
